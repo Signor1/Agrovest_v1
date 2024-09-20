9557fdfdc179ef5ffec3a0181a60e7b2fd6212a5
signor1 <emmanuelomemgboji@gmail.com>
2024-09-20 18:08:13 +0100

    feat: added asset files

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
 import { Montserrat as FontSans } from "next/font/google";
 import "../styles/globals.css";
 import { cn } from "@/lib/utils";
 import { Providers } from "./providers";
 import { Toaster } from "sonner";
+import { getMetadata } from "@/utils/getMetadata";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export const metadata: Metadata = {
+export const metadata = getMetadata({
   title: "AgroVest",
   description: "Tokenize your business, attract investors, while showcasing your products on a thriving marketplace. ",
-};
+});
 
 export default function RootLayout({
   children,
