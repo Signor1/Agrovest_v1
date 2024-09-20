1224340195acf5a29726cde7e2281aef5fc58875
signor1 <emmanuelomemgboji@gmail.com>
2024-09-20 19:13:02 +0100

    feat: added loader

---
+++ frontend/components/shared/ProgressLoader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { InfinitySpin } from 'react-loader-spinner'
+
+const ProgressLoader = () => {
+    return (
+        <div className="fixed top-0 left-0 w-full h-screen z-[9999] bg-white flex justify-center items-center">
+            <InfinitySpin
+                width="200"
+                color="#02390F"
+            />
+        </div>
+    )
+}
+
+export default ProgressLoader
