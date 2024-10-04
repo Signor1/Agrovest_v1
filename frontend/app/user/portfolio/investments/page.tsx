0042bedab9703a2f42b9e484217f32e00ecc94f1
signor1 <emmanuelomemgboji@gmail.com>
2024-10-04 01:48:56 +0100

    feat: added 2 sub routes

---
+++ frontend/app/user/portfolio/investments/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import type { NextPage } from "next"
+
+const PortfolioInvestments: NextPage = () => {
+    return (
+        <main className="w-full flex flex-col overflow-x-hidden">
+
+        </main>
+    )
+}
+
+export default PortfolioInvestments
