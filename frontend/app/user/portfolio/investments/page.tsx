2bba1025ea3b0ad651a4c3c43450e8b296a59666
signor1 <emmanuelomemgboji@gmail.com>
2024-10-04 02:49:24 +0100

    feat: added investments & products subroutes in portfolio

--- frontend/app/user/portfolio/investments/page.tsx
+++ frontend/app/user/portfolio/investments/page.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import type { NextPage } from "next"
+import Investments from "@/components/dashboard/portfolioSubRoutes/Investments"
 
 const PortfolioInvestments: NextPage = () => {
     return (
         <main className="w-full flex flex-col overflow-x-hidden">
-
+            <Investments />
         </main>
     )
 }
--- frontend/app/user/portfolio/products/page.tsx
+++ frontend/app/user/portfolio/products/page.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import type { NextPage } from "next"
+import Products from "@/components/dashboard/portfolioSubRoutes/Products"
 
 const PortfolioProducts: NextPage = () => {
     return (
         <main className="w-full flex flex-col overflow-x-hidden">
-
+            <Products />
         </main>
     )
 }
